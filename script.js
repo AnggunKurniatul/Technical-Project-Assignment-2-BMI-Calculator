bd31d931c2586489de13de45be51d805e428c98f
AnggunKurniatul <aggnkurnkurn15@gmail.com>
2023-04-11 21:34:39 +0700

    Edit index.html and script.js for create the output

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.getElementById('calculate').addEventListener('click', function(event){
+    event.preventDefault()
+
     const weight = document.getElementById('weight').value;
     const height = document.getElementById('height').value;
+
+    document.getElementById('output').innerHTML = 
+    '<p> Your BMI is <strong id="result"></strong> Your category is <strong id="category"></strong></p>';
 
     const total = document.getElementById('result');
     const category = document.getElementById('category');
@@ -16,7 +21,14 @@ document.getElementById('calculate').addEventListener('click', function(event){
         category.innerText = 'Normal weight'
     }else if(bmiCalculator <= 29.9){
         category.innerText = 'Overweight'
-    }else{
+    }else if(bmiCalculator > 29.9){
         category.innerText = 'Obesity'
+    }else{
+        alert('Enter your weight and height')
+        document.getElementById('output').innerHTML =""
     }
 });
+
+document.getElementById('reset').addEventListener('click', function(event){
+    form.reset()
+})
